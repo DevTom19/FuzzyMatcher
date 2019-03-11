ba37605bf991088a9552560bfc29e322571afb30
DevTom19 <48443607+DevTom19@users.noreply.github.com>
2019-03-11 15:01:05 +0000

    No colour

--- FuzzyMatch/Program.cs
+++ FuzzyMatch/Program.cs
@@ -17,19 +17,19 @@ namespace FuzzyMatch
                 
                 Console.WriteLine("Welcome to the Fuzzy Match testing application!");
                 Console.WriteLine("You have two options:");
-                Console.BackgroundColor = ConsoleColor.Blue;
+                //Console.BackgroundColor = ConsoleColor.Blue;
                 Console.WriteLine("1: Fuzzy search for a pattern within a string");
                 Console.WriteLine("2: fuzzy search for a pattern within a string, gives score");
-                Console.BackgroundColor = ConsoleColor.Red;
+                //Console.BackgroundColor = ConsoleColor.Red;
                 Console.WriteLine("Key in the menu item now.");
-                Console.BackgroundColor = ConsoleColor.Black;
+                //Console.BackgroundColor = ConsoleColor.Black;
                 int.TryParse(Console.ReadKey().KeyChar.ToString(), out int keyPress);
-                Console.BackgroundColor = ConsoleColor.Green;
+                //Console.BackgroundColor = ConsoleColor.Green;
                 Console.WriteLine("Please enter the string to search:");
                 var stringToSearch = Console.ReadLine();
                 Console.WriteLine("Please enter the pattern:");
                 var pattern = Console.ReadLine();
-                Console.BackgroundColor = ConsoleColor.White;
+                //Console.BackgroundColor = ConsoleColor.White;
                 switch (keyPress)
                 {
                     case 1:
@@ -46,7 +46,7 @@ namespace FuzzyMatch
                         Console.WriteLine("Did not recognise the menu item choice.");
                         break;
                 }
-                Console.BackgroundColor = ConsoleColor.Black;
+                //Console.BackgroundColor = ConsoleColor.Black;
                 Console.WriteLine("Press any key to continue, to exit key in 'e'");
                 var key = Console.ReadKey();
                 if (key.KeyChar == 'e') break;
